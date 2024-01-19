8fa25f440348e7e36b8827f12b93e6845d33b684
Alan Franco <alanfrancob97@gmail.com>
2024-01-18 21:58:32 -0600

    Firebase Auth Service Added Firebase Authentication service added with signup, login and signout methods.

---
+++ PostresUAMMApi/Services/FirebaseAuthService.cs
@@ -0,0 +1,24 @@
+﻿using Firebase.Auth;
+
+namespace PostresUAMMApi.Services;
+
+public class FirebaseAuthService(FirebaseAuthClient firebaseAuthClient)
+{
+    private readonly FirebaseAuthClient _firebaseAuthClient = firebaseAuthClient;
+
+    public async Task<string> SignUp(string email, string password)
+    {
+        UserCredential userCredential = await _firebaseAuthClient.CreateUserWithEmailAndPasswordAsync(email, password);
+
+        return await userCredential.User.GetIdTokenAsync();
+    }
+
+    public async Task<string> Login(string email, string password)
+    {
+        UserCredential userCredential = await _firebaseAuthClient.SignInWithEmailAndPasswordAsync(email, password);
+
+        return await userCredential.User.GetIdTokenAsync();
+    }
+
+    public void SignOut() => _firebaseAuthClient.SignOut();
+}
