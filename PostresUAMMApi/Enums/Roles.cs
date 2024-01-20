4567e0184193b363a57c4af1224114061e530e19
Alan Franco <alanfrancob97@gmail.com>
2024-01-20 17:39:41 -0600

    Roles enum added Roles enumeration was perferred over Role model for simplicity.

---
+++ PostresUAMMApi/Enums/Roles.cs
@@ -0,0 +1,13 @@
+﻿using Google.Cloud.Firestore;
+
+namespace PostresUAMMApi.Enums;
+
+[FirestoreData(ConverterType = typeof(FirestoreEnumNameConverter<RolesEnum>))]
+public enum RolesEnum
+{
+    None,
+    Admin,
+    Baker,
+    Customer,
+    Seller
+}
