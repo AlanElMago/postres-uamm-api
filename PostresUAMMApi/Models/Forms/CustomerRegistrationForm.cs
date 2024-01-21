159c4204313c046ce1afb21863070dcdc45186ff
Alan Franco <alanfrancob97@gmail.com>
2024-01-21 13:44:25 -0600

    Customer Registration Form model added Added model for Customer Registration Form for signup.

---
+++ PostresUAMMApi/Models/Forms/CustomerRegistrationForm.cs
@@ -0,0 +1,10 @@
+﻿namespace PostresUAMMApi.Models.Forms;
+
+public class CustomerRegistrationForm
+{
+    public string? Email { get; set; }
+
+    public string? Password { get; set; }
+
+    public string? FullName { get; set; }
+}
