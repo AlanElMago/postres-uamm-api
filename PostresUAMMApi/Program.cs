a2e7314a5559209a9f211e6290e573e0afc7ce93
Alan Franco <alanfrancob97@gmail.com>
2024-01-20 23:55:28 -0600

    Main program modified Modified main program to include dependencies, services and authentication.

--- PostresUAMMApi/Program.cs
+++ PostresUAMMApi/Program.cs
@@ -1,13 +1,50 @@
-var builder = WebApplication.CreateBuilder(args);
+using Firebase.Auth;
+using Firebase.Auth.Providers;
+using Google.Cloud.Firestore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using PostresUAMMApi.Repositories;
+using PostresUAMMApi.Services;
 
-// Set environmental variables
+var builder = WebApplication.CreateBuilder(args);
+string firebaseProjectName = "postres-uamm-firebase";
 string pathToFirebaseSecretKey = Path.Combine(
     "Secrets",
     "postres-uamm-firebase-firebase-adminsdk-ocw9l-fdf9dfbb56.json");
 
 Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", pathToFirebaseSecretKey);
 
+builder.Configuration.AddUserSecrets("382d69e6-8486-43ba-a6b4-edb9cde00db0");
+
+builder.Services.AddSingleton(new FirebaseAuthClient(new FirebaseAuthConfig
+{
+    ApiKey = builder.Configuration["FirebaseWebApiKey"],
+    AuthDomain = $"{firebaseProjectName}.firebaseapp.com",
+    Providers = new FirebaseAuthProvider[] { new EmailProvider() }
+}));
+
+builder.Services.AddSingleton(new FirestoreDbBuilder { ProjectId = firebaseProjectName }.Build());
+
 // Add services to the container.
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
+
+builder.Services.AddScoped<IFirebaseAuthService, FirebaseAuthService>();
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<ICustomerService, CustomerService>();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.Authority = $"https://securetoken.google.com/{firebaseProjectName}";
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidIssuer = $"https://securetoken.google.com/{firebaseProjectName}",
+        ValidateAudience = true,
+        ValidAudience = firebaseProjectName,
+        ValidateLifetime = true
+    };
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
