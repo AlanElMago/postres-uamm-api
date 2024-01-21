4edad459a24953b72c6b13741c8211b75c37a988
Alan Franco <alanfrancob97@gmail.com>
2024-01-20 23:48:48 -0600

    User service added Added User service to interface with User repository.

---
+++ PostresUAMMApi/Services/UserService.cs
@@ -0,0 +1,23 @@
+﻿using PostresUAMMApi.Models;
+using PostresUAMMApi.Repositories;
+
+namespace PostresUAMMApi.Services;
+
+public interface IUserService
+{
+    Task<User> RegisterUserAsync(User user);
+}
+
+public class UserService(IUserRepository userRepository) : IUserService
+{
+    private readonly IUserRepository _userRepository = userRepository;
+
+    public async Task<User> RegisterUserAsync(User user)
+    {
+        ArgumentNullException.ThrowIfNull(user);
+
+        User newUser = await _userRepository.AddUserAsync(user);
+
+        return newUser;
+    }
+}
