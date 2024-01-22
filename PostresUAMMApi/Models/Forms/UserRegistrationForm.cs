404f4bf4ab14245dca13aae2bf8968362b1c348d
Alan Franco <alanfrancob97@gmail.com>
2024-01-21 19:04:13 -0600

    UserRegistrationFrom modified Added data annotations to the UserRegistrationForm class.

--- PostresUAMMApi/Models/Forms/UserRegistrationForm.cs
+++ PostresUAMMApi/Models/Forms/UserRegistrationForm.cs
@@ -1,10 +1,16 @@
-﻿namespace PostresUAMMApi.Models.Forms;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PostresUAMMApi.Models.Forms;
 
 public class UserRegistrationForm
 {
+    [Required]
+    [EmailAddress]
     public string? Email { get; set; }
 
+    [Required]
     public string? Password { get; set; }
 
+    [Required]
     public string? FullName { get; set; }
 }
