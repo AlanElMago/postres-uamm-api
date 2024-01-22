9c41fd0df470f70fb8ce3c6dd80b2364246b2e8b
Alan Franco <alanfrancob97@gmail.com>
2024-01-21 18:32:29 -0600

    CampusLocation model and repo added Added CampusLocation model and repository.

---
+++ PostresUAMMApi/Models/CampusLocation.cs
@@ -0,0 +1,16 @@
+﻿using Google.Cloud.Firestore;
+
+namespace PostresUAMMApi.Models;
+
+[FirestoreData]
+public class CampusLocation
+{
+    [FirestoreDocumentId]
+    public string? Id { get; set; }
+
+    [FirestoreProperty(Name = "name")]
+    public string? Name { get; set; }
+
+    [FirestoreProperty(Name = "isIndoors")]
+    public bool IsIndoors { get; set; }
+}
---
+++ PostresUAMMApi/Repositories/CampusLocationRepository.cs
@@ -0,0 +1,45 @@
+﻿using Google.Cloud.Firestore;
+using Google.Cloud.Firestore.V1;
+using PostresUAMMApi.Models;
+
+namespace PostresUAMMApi.Repositories;
+
+public interface ICampusLocationRepository
+{
+    Task<List<CampusLocation>> GetCampusLocationsAsync();
+
+    Task<CampusLocation?> GetCampusLocationAsync(string id);
+}
+
+public class CampusLocationRepository(FirestoreDb firestoreDb) : ICampusLocationRepository
+{
+    private readonly FirestoreDb _firestoreDb = firestoreDb;
+
+    public async Task<List<CampusLocation>> GetCampusLocationsAsync()
+    {
+        Query campusLocationsCollQuery = _firestoreDb.Collection("campusLocations");
+        QuerySnapshot allCampusLocationsQuerySnapshot = await campusLocationsCollQuery.GetSnapshotAsync();
+        List<CampusLocation> campusLocations = allCampusLocationsQuerySnapshot
+            .Select(campusLocationDocSnapshot => campusLocationDocSnapshot.ConvertTo<CampusLocation>())
+            .ToList();
+
+        return campusLocations;
+    }
+
+    public async Task<CampusLocation?> GetCampusLocationAsync(string id)
+    {
+        DocumentSnapshot campusLocationDocSnapshot = await _firestoreDb
+            .Collection("campusLocations")
+            .Document(id)
+            .GetSnapshotAsync();
+
+        if (!campusLocationDocSnapshot.Exists)
+        {
+            throw new InvalidOperationException($"CampusLocation with id {id} does not exist in the database");
+        }
+
+        CampusLocation campusLocation = campusLocationDocSnapshot.ConvertTo<CampusLocation>();
+
+        return campusLocation;
+    }
+}
